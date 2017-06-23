0c68b2d0975ecce7552a051dec78a9cbf963134d
Brian Thomson <brian.thomson@oracle.com>
2017-06-23 13:07:57 -0700

    Replaced IOL RSS feed with that from the Financial Times.

--- headlines.py
+++ headlines.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
-             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+             'ft': 'https://www.ft.com/?edition=international&format=rss'}
 
 @app.route('/')
 @app.route('/<publication>')
