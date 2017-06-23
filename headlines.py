75b59ef0a61b1a0fbe4c15b5ba5e493cf78e1618
Brian Thomson <brian.thomson@oracle.com>
2017-06-23 13:42:42 -0700

    Added Jinja templating.

--- headlines.py
+++ headlines.py
@@ -1,6 +1,7 @@
 # headlines.py
 import feedparser
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -15,18 +16,12 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication='bbc'):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    html_str = '''
-    <html>
-        <body>
-            <h1> Headlines </h1>
-            <b>{0}</b> <br/>
-            <i>{1}</i> <br/>
-            <p>{2}</p> <br/>
-        </body>
-    </html> '''
-    return html_str.format(first_article.get("title"),
-                           first_article.get("published"),
-                           first_article.get("summary"))
+
+    return render_template('home.html',
+                           title=first_article.get('title'),
+                           published=first_article.get("published"),
+                           summary=first_article.get("summary"))
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
